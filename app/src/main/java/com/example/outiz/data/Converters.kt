e30e11eae750909c6bfe436ec6b0ae7b7578b428
DaMolks <108116237+DaMolks@users.noreply.github.com>
2024-12-26 16:29:40 +0100

    Ajout des TypeConverters pour Room

---
+++ app/src/main/java/com/example/outiz/data/Converters.kt
@@ -0,0 +1,26 @@
+package com.example.outiz.data
+
+import androidx.room.TypeConverter
+import java.util.Date
+
+class Converters {
+    @TypeConverter
+    fun fromTimestamp(value: Long?): Date? {
+        return value?.let { Date(it) }
+    }
+
+    @TypeConverter
+    fun dateToTimestamp(date: Date?): Long? {
+        return date?.time
+    }
+
+    @TypeConverter
+    fun fromString(value: String?): List<String> {
+        return value?.split(",")?.filter { it.isNotEmpty() } ?: emptyList()
+    }
+
+    @TypeConverter
+    fun toString(list: List<String>?): String {
+        return list?.joinToString(",") ?: ""
+    }
+}

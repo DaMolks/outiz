77943359f7cca34d08462b09bed1c10e78f209b7
DaMolks <108116237+DaMolks@users.noreply.github.com>
2024-12-27 19:36:04 +0100

    build: Correction de la configuration Safe Args

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
 buildscript {
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.6")
+    }
     repositories {
         google()
         mavenCentral()
-    }
-    dependencies {
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.6")
     }
 }
 

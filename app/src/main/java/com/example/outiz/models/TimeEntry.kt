1d7d56ed4ce16ac3b9b47031193eb888adbe3e12
DaMolks <108116237+DaMolks@users.noreply.github.com>
2024-12-27 20:11:25 +0100

    Mise à jour du modèle TimeEntry pour inclure la date et la durée

--- app/src/main/java/com/example/outiz/models/TimeEntry.kt
+++ app/src/main/java/com/example/outiz/models/TimeEntry.kt
@@ -5,6 +5,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 import java.time.LocalDateTime
+import java.util.Date
 
 @Parcelize
 @Entity(tableName = "time_entries")
@@ -12,6 +13,8 @@ data class TimeEntry(
     @PrimaryKey(autoGenerate = true)
     val id: Long = 0,
     val reportId: String,
+    val date: Date,
+    val duration: Int,
     val startTime: LocalDateTime,
     val endTime: LocalDateTime,
     val description: String,

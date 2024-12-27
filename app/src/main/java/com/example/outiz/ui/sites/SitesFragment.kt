fe92f0cbc44e1683bb2c9c31d6bb5e2171415a05
DaMolks <108116237+DaMolks@users.noreply.github.com>
2024-12-27 19:37:02 +0100

    fix: Utilisation directe de l'ID de navigation

--- app/src/main/java/com/example/outiz/ui/sites/SitesFragment.kt
+++ app/src/main/java/com/example/outiz/ui/sites/SitesFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.outiz.R
 import com.example.outiz.databinding.FragmentSitesBinding
 
 class SitesFragment : Fragment() {
@@ -41,7 +42,7 @@ class SitesFragment : Fragment() {
 
     private fun setupListeners() {
         binding.fabAddSite.setOnClickListener {
-            findNavController().navigate(SitesFragmentDirections.actionSitesFragmentToEditSiteFragment())
+            findNavController().navigate(R.id.action_sitesFragment_to_editSiteFragment)
         }
     }
 

9d34023b9b9d52b13b2974a54ffe2f6884b4ac04
DaMolks <108116237+DaMolks@users.noreply.github.com>
2024-12-27 19:13:33 +0100

    feat: Ajout de ReportPagerAdapter

--- app/src/main/java/com/example/outiz/ui/reports/ReportPagerAdapter.kt
+++ app/src/main/java/com/example/outiz/ui/reports/ReportPagerAdapter.kt
@@ -3,18 +3,18 @@ package com.example.outiz.ui.reports
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.outiz.ui.reports.tabs.ReportInfoFragment
-import com.example.outiz.ui.reports.tabs.ReportPhotosFragment
+import com.example.outiz.ui.reports.tabs.ReportPhotoFragment
 import com.example.outiz.ui.reports.tabs.ReportTimeFragment
 
 class ReportPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
 
-    override fun getItemCount() = 3
+    override fun getItemCount(): Int = 3
 
     override fun createFragment(position: Int): Fragment {
         return when (position) {
             0 -> ReportInfoFragment()
             1 -> ReportTimeFragment()
-            2 -> ReportPhotosFragment()
+            2 -> ReportPhotoFragment()
             else -> throw IllegalArgumentException("Invalid position $position")
         }
     }

e93c8159ae5252b20440ce6be04533f82f5e954f
DaMolks <108116237+DaMolks@users.noreply.github.com>
2024-12-27 19:22:49 +0100

    feat: Ajout de EditReportPagerAdapter

---
+++ app/src/main/java/com/example/outiz/ui/reports/EditReportPagerAdapter.kt
@@ -0,0 +1,20 @@
+package com.example.outiz.ui.reports
+
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.outiz.ui.reports.tabs.ReportInfoFragment
+import com.example.outiz.ui.reports.tabs.ReportPhotoFragment
+import com.example.outiz.ui.reports.tabs.ReportTimeFragment
+
+class EditReportPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
+    override fun getItemCount(): Int = 3
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> ReportInfoFragment()
+            1 -> ReportTimeFragment()
+            2 -> ReportPhotoFragment()
+            else -> throw IllegalStateException("Invalid position $position")
+        }
+    }
+}

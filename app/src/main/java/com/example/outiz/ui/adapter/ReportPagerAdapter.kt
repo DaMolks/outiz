f5b42bf0c68045b825869d7ab671f2e1f979e264
DaMolks <108116237+DaMolks@users.noreply.github.com>
2024-12-27 12:14:19 +0100

    Ajout du ReportPagerAdapter

---
+++ app/src/main/java/com/example/outiz/ui/adapter/ReportPagerAdapter.kt
@@ -0,0 +1,22 @@
+package com.example.outiz.ui.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.outiz.ui.fragments.InformationFragment
+import com.example.outiz.ui.fragments.PhotosFragment
+import com.example.outiz.ui.fragments.TimeTrackingFragment
+
+class ReportPagerAdapter(fragment: Fragment, private val reportId: Long) : 
+    FragmentStateAdapter(fragment) {
+
+    override fun getItemCount(): Int = 3
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> InformationFragment.newInstance(reportId)
+            1 -> TimeTrackingFragment.newInstance(reportId)
+            2 -> PhotosFragment.newInstance(reportId)
+            else -> throw IllegalStateException("Position invalide $position")
+        }
+    }
+}

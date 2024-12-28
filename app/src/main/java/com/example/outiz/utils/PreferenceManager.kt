32e114de477917520568ee1f312679658a7667f4
DaMolks <108116237+DaMolks@users.noreply.github.com>
2024-12-28 06:18:34 +0100

    🛡️ Correction de PreferenceManager

--- app/src/main/java/com/example/outiz/utils/PreferenceManager.kt
+++ app/src/main/java/com/example/outiz/utils/PreferenceManager.kt
@@ -4,11 +4,11 @@ import android.content.Context
 import androidx.preference.PreferenceManager
 
 class AppPreferenceManager(context: Context) {
-    private val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
+    private val preferences = PreferenceManager.getDefaultSharedPreferences(context)
 
     var isTechnicianProfileCreated: Boolean
-        get() = sharedPreferences.getBoolean(KEY_TECHNICIAN_PROFILE, false)
-        set(value) = sharedPreferences.edit().putBoolean(KEY_TECHNICIAN_PROFILE, value).apply()
+        get() = preferences.getBoolean(KEY_TECHNICIAN_PROFILE, false)
+        set(value) = preferences.edit().putBoolean(KEY_TECHNICIAN_PROFILE, value).apply()
 
     companion object {
         private const val KEY_TECHNICIAN_PROFILE = "technician_profile_created"

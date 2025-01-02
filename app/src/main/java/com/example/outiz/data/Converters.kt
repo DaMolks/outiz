c2f63ffcc77e8e0691a2445d5d0fa5ae4169d21c
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-02 11:33:27 +0100

    feat: Add list type converters for Room

--- app/src/main/java/com/example/outiz/data/Converters.kt
+++ app/src/main/java/com/example/outiz/data/Converters.kt
@@ -1,10 +1,14 @@
 package com.example.outiz.data
 
 import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import java.time.LocalDateTime
 import java.time.ZoneOffset
 
 class Converters {
+    private val gson = Gson()
+
     @TypeConverter
     fun fromTimestamp(value: Long?): LocalDateTime? {
         return value?.let { LocalDateTime.ofEpochSecond(it, 0, ZoneOffset.UTC) }
@@ -13,5 +17,17 @@ class Converters {
     @TypeConverter
     fun dateToTimestamp(date: LocalDateTime?): Long? {
         return date?.toEpochSecond(ZoneOffset.UTC)
+    }
+
+    @TypeConverter
+    fun fromStringList(value: String?): List<String>? {
+        if (value == null) return null
+        val listType = object : TypeToken<List<String>>() {}.type
+        return gson.fromJson(value, listType)
+    }
+
+    @TypeConverter
+    fun stringListToString(list: List<String>?): String? {
+        return list?.let { gson.toJson(it) }
     }
 }

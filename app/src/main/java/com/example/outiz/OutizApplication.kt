df72a95ab3357f9c801231056a98fa36e7f2501f
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-02 11:39:07 +0100

    feat: Add Hilt application class

--- app/src/main/java/com/example/outiz/OutizApplication.kt
+++ app/src/main/java/com/example/outiz/OutizApplication.kt
@@ -1,10 +1,7 @@
 package com.example.outiz
 
 import android.app.Application
-import com.example.outiz.data.OutizDatabase
-import com.example.outiz.utils.AppPreferenceManager
+import dagger.hilt.android.HiltAndroidApp
 
-class OutizApplication : Application() {
-    val database by lazy { OutizDatabase.getDatabase(this) }
-    val preferenceManager by lazy { AppPreferenceManager(this) }
-}
+@HiltAndroidApp
+class OutizApplication : Application()

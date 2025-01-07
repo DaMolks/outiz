f4faf840abd7646fa09f47f64b88516e7e49396f
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-07 18:52:05 +0100

    Fix ExceptionHandler file structure

--- app/src/main/java/com/example/outiz/utils/ExceptionHandler.kt
+++ app/src/main/java/com/example/outiz/utils/ExceptionHandler.kt
@@ -1,25 +1,12 @@
 package com.example.outiz.utils
 
-import android.util.Log
-import com.example.outiz.BuildConfig
 import timber.log.Timber
-kotlin.system.exitProcess
 
-class ExceptionHandler : Thread.UncaughtExceptionHandler {
-    private val defaultHandler = Thread.getDefaultUncaughtExceptionHandler()
-
-    override fun uncaughtException(thread: Thread, throwable: Throwable) {
-        try {
-            Timber.e(throwable)
-            if (!BuildConfig.DEBUG) {
-                Log.e("Outiz", "Fatal error:", throwable)
-                // Ici, on pourrait envoyer le rapport à un service de crash reporting
-            }
-        } catch (e: Exception) {
-            // Assurons-nous que nous ne créons pas une boucle d'exceptions
-            Log.e("Outiz", "Error reporting crash", e)
-        } finally {
-            defaultHandler?.uncaughtException(thread, throwable) ?: exitProcess(1)
+class ExceptionHandler private constructor() {
+    companion object {
+        fun handleException(e: Throwable) {
+            Timber.e(e, "Unhandled exception")
+            // Add additional error handling logic here
         }
     }
 }

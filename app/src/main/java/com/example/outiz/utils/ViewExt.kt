1b31adcfe1c670046b0623d6a383bf263b65cb7a
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-07 17:48:26 +0100

    Extension des méthodes View

--- app/src/main/java/com/example/outiz/utils/ViewExt.kt
+++ app/src/main/java/com/example/outiz/utils/ViewExt.kt
@@ -1,14 +1,22 @@
 package com.example.outiz.utils
 
+import android.content.Context
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import androidx.core.view.isVisible
 import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
 
-fun View.show() {
-    visibility = View.VISIBLE
+fun View.showKeyboard() {
+    requestFocus()
+    val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
 }
 
-fun View.hide() {
-    visibility = View.GONE
+fun View.hideKeyboard() {
+    val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.hideSoftInputFromWindow(windowToken, 0)
 }
 
 fun View.showSnackbar(message: String, duration: Int = Snackbar.LENGTH_SHORT) {
@@ -24,4 +32,24 @@ fun View.showSnackbarWithAction(
     Snackbar.make(this, message, duration)
         .setAction(actionText) { action() }
         .show()
+}
+
+fun EditText.showError(error: String?) {
+    if (error != null) {
+        (parent.parent as? TextInputLayout)?.error = error
+    } else {
+        (parent.parent as? TextInputLayout)?.error = null
+    }
+}
+
+fun View.setVisible(visible: Boolean, useInvisible: Boolean = false) {
+    visibility = when {
+        visible -> View.VISIBLE
+        useInvisible -> View.INVISIBLE
+        else -> View.GONE
+    }
+}
+
+fun View.toggleVisibility() {
+    isVisible = !isVisible
 }

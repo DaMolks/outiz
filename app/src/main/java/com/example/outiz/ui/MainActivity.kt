0bb4b6fe45498bcea18ececbf2ab6eca814996e3
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-09 16:11:27 +0100

    feat: Ajout de la MainActivity

---
+++ app/src/main/java/com/example/outiz/ui/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.outiz.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.outiz.R
+import com.example.outiz.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val navController by lazy {
+        (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment)
+            .navController
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupNavigation()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    private fun setupNavigation() {
+        setSupportActionBar(binding.toolbar)
+        setupActionBarWithNavController(navController)
+
+        // Observer les changements de destination pour mettre à jour le titre
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            binding.toolbar.title = destination.label
+        }
+    }
+}

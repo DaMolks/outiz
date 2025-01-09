3dd615536fb78267782743478630c19e0f700ed7
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-09 15:57:30 +0100

    fix: Standardisation de l'utilisation de Flow dans SiteRepository

--- app/src/main/java/com/example/outiz/data/repository/SiteRepository.kt
+++ app/src/main/java/com/example/outiz/data/repository/SiteRepository.kt
@@ -1,20 +1,20 @@
 package com.example.outiz.data.repository
 
-import androidx.lifecycle.LiveData
 import com.example.outiz.data.dao.SiteDao
 import com.example.outiz.models.Site
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class SiteRepository @Inject constructor(
     private val siteDao: SiteDao
 ) {
-    val allSites: LiveData<List<Site>> = siteDao.getAllSites()
+    val allSites: Flow<List<Site>> = siteDao.getAllSites()
 
-    suspend fun insert(site: Site) = siteDao.insertSite(site)
+    suspend fun insert(site: Site): Long = siteDao.insertSite(site)
 
     suspend fun update(site: Site) = siteDao.updateSite(site)
 
     suspend fun delete(site: Site) = siteDao.deleteSite(site)
 
-    suspend fun getSiteById(id: Long): Site? = siteDao.getSiteById(id)
+    fun getSiteById(id: Long): Flow<Site?> = siteDao.getSiteById(id)
 }

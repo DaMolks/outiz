54ff3e4e349d5b56e67fa8273b53361f648885a8
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-09 16:10:00 +0100

    fix: Configuration de l'application principale avec Hilt

--- app/src/main/java/com/example/outiz/OutizApplication.kt
+++ app/src/main/java/com/example/outiz/OutizApplication.kt
@@ -1,18 +1,14 @@
 package com.example.outiz
 
 import android.app.Application
-import com.example.outiz.utils.CrashReportingTree
-import dagger.hilt.android.HiltAndroidApp
-import timber.log.Timber
+import com.example.outiz.utils.Logger
+dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class OutizApplication : Application() {
+
     override fun onCreate() {
         super.onCreate()
-        if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
-        } else {
-            Timber.plant(CrashReportingTree())
-        }
+        Logger.init()
     }
 }
